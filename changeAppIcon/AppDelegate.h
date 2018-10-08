fb4dca7ed265051549738af84ecc2eceb6a08361
wangchenglong <wangchenglong@sdjictec.com>
2018-10-08 09:13:12 +0800

    Initial Commit

---
+++ changeAppIcon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  changeAppIcon
+//
+//  Created by 王成龙 on 2018/10/8.
+//  Copyright © 2018年 CL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ changeAppIcon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  changeAppIcon
+//
+//  Created by 王成龙 on 2018/10/8.
+//  Copyright © 2018年 CL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
